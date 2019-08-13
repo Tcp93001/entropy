f974ec615cb7286a5b973b39fd60d59d02b2e21c
wicho1001 <wichoguerrero@yahoo.com>
2019-08-12 20:49:06 -0500

    Adding first set of components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss'
 
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
 function App() {
   return (
     <div className={styles.app}>
-      <header className="App-header">
+      <header className={styles.app_header}>
         <img src={logo} className={styles.appuy} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.app_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+
+        <Button>Texto</Button>
+
+        <Form.Control className={styles.select} as='select'>
+          <option>Mostrar 20 resultados</option>
+          <option>Mostrar 30</option>
+          <option>Mostrar 40</option>
+          <option>Mostrar 50</option>
+          <option>Mostrar 100</option>
+        </Form.Control>
       </header>
     </div>
   );
---
+++ src/components/SelectSize/SelectSize.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SelectSize.scss'
+
+export const SelectSize = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
